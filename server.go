fbb4f6e719b6b6775fd408a361b3f4d9f2297e7b
Patrick D'appollonio <patrick@dappollonio.us>
2016-03-30 00:29:36 -0400

    Agrega Godoc. Hace formateo general.

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 	"github.com/googollee/go-socket.io"
 )
 
-//PERMITE CORS
+// Función middleware agrega cabeceras CORS a cada solicitud
 func middleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		if origin := req.Header.Get("Origin"); origin != "" {
@@ -22,14 +22,20 @@ func middleware(h http.Handler) http.Handler {
 	})
 }
 
-//////////////////////////////////////////////
+// Estructura cliente maneja los IDs de cliente y
+// de socket de un cliente conectado
 type Cliente struct {
 	id        string
 	socket_id string
 }
 
+// Variable CLIENTES contiene un listado de todos
+// los clientes encontrados
 var CLIENTES []*Cliente
 
+// Función buscar retorna -1 si el ID no se encontró
+// en el listado de clientes o la posición en el slice
+// si se encontró
 func buscar(id string) int {
 	var index = -1
 	for i := 0; i < len(CLIENTES); i++ {
@@ -40,90 +46,71 @@ func buscar(id string) int {
 	return index
 }
 
-//////////////////////////////////////////////
 func main() {
 	server, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	server.On("connection", func(so socketio.Socket) {
 		log.Println("CONNECTED key:" + so.Id())
 
-		so.On("loginMonitor", func(monitor string) { //no esta implementado response
-
+		so.On("loginMonitor", func(monitor string) {
 			if monitor == "monitor" {
-				//Add Room
 				so.Join("monitores")
-				so.Emit("loginMonitorResponse", string(so.Id())) //este emit funciona como response
-
+				so.Emit("loginMonitorResponse", string(so.Id()))
 			}
 		})
-		////////////////////////////
+
 		so.On("loginCliente", func(id string) {
-			//Add Room
-			//so.Join("clientes")
-			//log.Println("LOGIN CLIENTE");
-
 			var index = buscar(id)
-			if index == -1 { //NO ENCONTRADO
-
+			if index == -1 {
 				var tmp Cliente
 				tmp.id = id
 				tmp.socket_id = so.Id()
 
-				//Add cliente al final del SLICE
 				CLIENTES = append(CLIENTES, &tmp)
-				//JSON.stringify
-				jsonData := map[string]string{"estado": "1", "id": id} //var info={estado:1,id:data.id};
+				jsonData := map[string]string{"estado": "1", "id": id}
 				var jsonStringify, _ = json.Marshal(jsonData)
 
 				so.Emit("loginClienteResponse", string(jsonStringify))
-
 			} else {
-
-				//JSON.stringify
-				jsonData := map[string]string{"estado": "0", "id": ""} //var info={estado:0,id:null};
+				jsonData := map[string]string{"estado": "0", "id": ""}
 				var jsonStringify, _ = json.Marshal(jsonData)
 
 				so.Emit("loginClienteResponse", string(jsonStringify))
-
 			}
-
 		})
-		////////////////////////////
+
 		so.On("posicionClientes", func(data string) {
 			log.Println(data)
-			//io.sockets.connected[SOCKET_MONITOR_ID].emit('monitorPrincipal',data);
 			so.BroadcastTo("monitores", "monitorPrincipal", data)
 		})
-		////////////////////////////
 
 		so.On("disconnection", func() {
 			log.Println("DISCONNECT key:" + so.Id())
 
 			for i := 0; i < len(CLIENTES); i++ {
 				if CLIENTES[i].socket_id == so.Id() {
-
-					//JSON.stringify
 					jsonData := map[string]string{"id": CLIENTES[i].id, "socket_id": CLIENTES[i].socket_id}
 					var jsonStringify, _ = json.Marshal(jsonData)
 
 					so.BroadcastTo("monitores", "monitorPrincipalDisconnet", string(jsonStringify))
 
-					//Removemos cliente del SLICE, sintaxis jodidamente especial
 					CLIENTES = append(CLIENTES[:i], CLIENTES[i+1:]...)
-
 				}
 			}
 
 		})
 	})
+
 	server.On("error", func(so socketio.Socket, err error) {
 		log.Println("error:", err)
 	})
 
 	http.Handle("/socket.io/", middleware(server))
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+
 	log.Println("Serving at localhost:9095...")
 	log.Fatal(http.ListenAndServe("127.0.0.1:9095", nil))
 }
