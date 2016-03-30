72787e9de346730ecd525939070135ea4095665c
Patrick D'appollonio <patrick@dappollonio.us>
2016-03-30 00:41:30 -0400

    Actualiza funciones, limpia un poco más. Hace el código más idiomático.

--- server.go
+++ server.go
@@ -8,12 +8,12 @@ import (
 	"github.com/googollee/go-socket.io"
 )
 
-// Función middleware agrega cabeceras CORS a cada solicitud
 func middleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		if origin := req.Header.Get("Origin"); origin != "" {
 			rw.Header().Set("Access-Control-Allow-Origin", origin)
 		}
+
 		rw.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
 		rw.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		rw.Header().Set("Access-Control-Allow-Credentials", "true")
@@ -33,19 +33,6 @@ type Cliente struct {
 // los clientes encontrados
 var CLIENTES []*Cliente
 
-// Función buscar retorna -1 si el ID no se encontró
-// en el listado de clientes o la posición en el slice
-// si se encontró
-func buscar(id string) int {
-	var index = -1
-	for i := 0; i < len(CLIENTES); i++ {
-		if CLIENTES[i].id == id {
-			return i
-		}
-	}
-	return index
-}
-
 func main() {
 	server, err := socketio.NewServer(nil)
 	if err != nil {
@@ -54,54 +41,10 @@ func main() {
 
 	server.On("connection", func(so socketio.Socket) {
 		log.Println("CONNECTED key:" + so.Id())
-
-		so.On("loginMonitor", func(monitor string) {
-			if monitor == "monitor" {
-				so.Join("monitores")
-				so.Emit("loginMonitorResponse", string(so.Id()))
-			}
-		})
-
-		so.On("loginCliente", func(id string) {
-			var index = buscar(id)
-			if index == -1 {
-				var tmp Cliente
-				tmp.id = id
-				tmp.socket_id = so.Id()
-
-				CLIENTES = append(CLIENTES, &tmp)
-				jsonData := map[string]string{"estado": "1", "id": id}
-				var jsonStringify, _ = json.Marshal(jsonData)
-
-				so.Emit("loginClienteResponse", string(jsonStringify))
-			} else {
-				jsonData := map[string]string{"estado": "0", "id": ""}
-				var jsonStringify, _ = json.Marshal(jsonData)
-
-				so.Emit("loginClienteResponse", string(jsonStringify))
-			}
-		})
-
-		so.On("posicionClientes", func(data string) {
-			log.Println(data)
-			so.BroadcastTo("monitores", "monitorPrincipal", data)
-		})
-
-		so.On("disconnection", func() {
-			log.Println("DISCONNECT key:" + so.Id())
-
-			for i := 0; i < len(CLIENTES); i++ {
-				if CLIENTES[i].socket_id == so.Id() {
-					jsonData := map[string]string{"id": CLIENTES[i].id, "socket_id": CLIENTES[i].socket_id}
-					var jsonStringify, _ = json.Marshal(jsonData)
-
-					so.BroadcastTo("monitores", "monitorPrincipalDisconnet", string(jsonStringify))
-
-					CLIENTES = append(CLIENTES[:i], CLIENTES[i+1:]...)
-				}
-			}
-
-		})
+		so.On("loginMonitor", loginMonitor(so))
+		so.On("loginCliente", loginCliente(so))
+		so.On("posicionClientes", posicionClientes(so))
+		so.On("disconnection", disconnection(so))
 	})
 
 	server.On("error", func(so socketio.Socket, err error) {
@@ -113,4 +56,78 @@ func main() {
 
 	log.Println("Serving at localhost:9095...")
 	log.Fatal(http.ListenAndServe("127.0.0.1:9095", nil))
+}
+
+func buscarEnSlice(id string) bool {
+	for _, val := range CLIENTES {
+		if val.id == id {
+			return true
+		}
+	}
+
+	return false
+}
+
+func loginMonitor(so socketio.Socket) func(monitor string) {
+	return func(monitor string) {
+		if monitor != "monitor" {
+			return
+		}
+
+		so.Join("monitores")
+		so.Emit("loginMonitorResponse", string(so.Id()))
+	}
+}
+
+func loginCliente(so socketio.Socket) func(id string) {
+	return func(id string) {
+		if buscarEnSlice(id) {
+			tmp := Cliente{id: id, socket_id: so.Id()}
+			CLIENTES = append(CLIENTES, &tmp)
+
+			jsonData := map[string]string{
+				"estado": "1",
+				"id":     id,
+			}
+
+			barray, _ := json.Marshal(jsonData)
+			so.Emit("loginClienteResponse", string(barray))
+			return
+		}
+
+		jsonData := map[string]string{
+			"estado": "0",
+			"id":     "",
+		}
+
+		barray, _ := json.Marshal(jsonData)
+		so.Emit("loginClienteResponse", string(barray))
+	}
+}
+
+func posicionClientes(so socketio.Socket) func(data string) {
+	return func(data string) {
+		log.Println(data)
+		so.BroadcastTo("monitores", "monitorPrincipal", data)
+	}
+}
+
+func disconnection(so socketio.Socket) func() {
+	return func() {
+		log.Println("DISCONNECT key:" + so.Id())
+
+		for i, cliente := range CLIENTES {
+			if cliente.socket_id == so.Id() {
+				jsonData := map[string]string{
+					"id":        cliente.id,
+					"socket_id": cliente.socket_id,
+				}
+
+				barray, _ := json.Marshal(jsonData)
+				so.BroadcastTo("monitores", "monitorPrincipalDisconnet", string(barray))
+
+				CLIENTES = append(CLIENTES[:i], CLIENTES[i+1:]...)
+			}
+		}
+	}
 }
